44a3dd4ae0251e5c53cd6b874517ecf874228209
Salim-agva <salim@agvahealthtech.comm>
2024-04-04 16:49:38 +0530

    Notification Clearall functionality, badge on icon, no notification image

--- lib/Screens/Common/NotificationScreen.dart
+++ lib/Screens/Common/NotificationScreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, sort_child_properties_last, curly_braces_in_flow_control_structures
 
 import 'package:agva_app/Screens/Doctor&Assistant/DoctorHomeScreen.dart';
 import 'package:agva_app/Screens/Doctor&Assistant/NurseHomeScreen.dart';
@@ -15,12 +15,11 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-  
   @override
   void initState() {
     super.initState();
     getUserType();
-    
+
     // notificationCounts = MessagingService.notifications.length;
     // print('notificationCounts inscreen $notificationCounts');
 
@@ -74,98 +73,136 @@ class _NotificationScreenState extends State<NotificationScreen> {
         centerTitle: true,
         title: Text("Notifications"),
       ),
-      body: 
-       StreamBuilder<Object>(
-         stream: FirebaseMessaging.onMessage,
-         builder: (context, snapshot) {
-           return ListView.builder(
-            itemCount: MessagingService.notifications.length,
-            itemBuilder: (context, index) {
-              // Access notification data
-              final notification = MessagingService.notifications[index];
-              final notificationData = notification.data;
-              final title = notification.notification!.title ?? "";
-              final body = notification.notification!.body ?? "";
-           
-              return Dismissible(
-                direction: DismissDirection.endToStart,
-                key: UniqueKey(),
-                onDismissed: (direction) {
-                  setState(() {
-                    MessagingService.notifications.removeAt(index);
-                  });
-                },
-                background: Container(
-                  color: Color.fromARGB(255, 202, 13, 0),
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
-                    size: 36,
-                  ),
-                  alignment: Alignment.centerRight,
-                  padding: EdgeInsets.only(right: 20),
-                ),
-                child: ListTile(
-                  title: Container(
-                    width: double.infinity,
-                    
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 55, 55, 55),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                title,
-                                style: TextStyle(
-                                  fontFamily: 'Avenir',
-                                  color: Color.fromARGB(255, 218, 218, 218),
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.035,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(
-                                height: MediaQuery.of(context).size.width * 0.02,
-                              ),
-                              SizedBox(
-                                width: 330,
-                                child: Text(
-                                  body,
-                                  style: TextStyle(
-                                      fontFamily: 'Avenir',
-                                      color: Color.fromARGB(255, 218, 218, 218),
-                                      fontSize:
-                                          MediaQuery.of(context).size.width * 0.03,
-                                      fontWeight: FontWeight.w200),
-                                ),
-                              ),
-                            ],
-                          ),
-                          // SizedBox(
-                          //     height: 20,
-                          //     child: Image.asset('assets/images/Logo.png'))
-                        ],
-                      ),
-                    ),
-                  ),
-           
-                  // title: Text(title),
-                  // subtitle: Text(body),
-                  onTap: () {
-                    _handleNotificationTap(context, notificationData);
-                  },
+      body: StreamBuilder<Object>(
+          stream: FirebaseMessaging.onMessage,
+          builder: (context, snapshot) {
+            if (MessagingService.notifications.length <= 0)
+              return Center(
+                child: Container(
+                  width: 220,
+                  child: Image.asset('assets/images/NoNotification.png'),
                 ),
               );
-            },
-                 );
-         }
-       ),
+            else
+              return ListView.builder(
+                  itemCount: MessagingService.notifications.length,
+                  itemBuilder: (context, index) {
+                    // Access notification data
+                    final notification = MessagingService.notifications[index];
+                    final notificationData = notification.data;
+                    final title = notification.notification!.title ?? "";
+                    final body = notification.notification!.body ?? "";
+
+                    return Dismissible(
+                      direction: DismissDirection.endToStart,
+                      key: UniqueKey(),
+                      onDismissed: (direction) {
+                        setState(() {
+                          MessagingService.notifications.removeAt(index);
+                        });
+                      },
+                      background: Container(
+                        color: Color.fromARGB(255, 202, 13, 0),
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                          size: 36,
+                        ),
+                        alignment: Alignment.centerRight,
+                        padding: EdgeInsets.only(right: 20),
+                      ),
+                      child: ListTile(
+                        title: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 55, 55, 55),
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      title,
+                                      style: TextStyle(
+                                        fontFamily: 'Avenir',
+                                        color:
+                                            Color.fromARGB(255, 218, 218, 218),
+                                        fontSize:
+                                            MediaQuery.of(context).size.width *
+                                                0.035,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.width *
+                                              0.02,
+                                    ),
+                                    SizedBox(
+                                      width: 330,
+                                      child: Text(
+                                        body,
+                                        style: TextStyle(
+                                            fontFamily: 'Avenir',
+                                            color: Color.fromARGB(
+                                                255, 218, 218, 218),
+                                            fontSize: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.03,
+                                            fontWeight: FontWeight.w200),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                // SizedBox(
+                                //     height: 20,
+                                //     child: Image.asset('assets/images/Logo.png'))
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        // title: Text(title),
+                        // subtitle: Text(body),
+                        onTap: () {
+                          _handleNotificationTap(context, notificationData);
+                        },
+                      ),
+                    );
+                  });
+          }
+          
+          
+          ),
+
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: StreamBuilder<Object>(
+        stream: FirebaseMessaging.onMessage,
+        builder: (context, snapshot) {
+          return Visibility(
+          visible: MessagingService.notifications.isNotEmpty,
+            child: SizedBox(
+              height: 35,
+              width: 100,
+              child: FloatingActionButton(
+                backgroundColor: const Color.fromARGB(137, 158, 158, 158),
+                onPressed: () {
+                  setState(() {
+                    MessagingService.notifications.clear();
+                  });
+                },
+                child: Text('Clear',style: TextStyle(fontWeight: FontWeight.w400),),
+              ),
+            ),
+          );
+        }
+      ),
     );
   }
 
